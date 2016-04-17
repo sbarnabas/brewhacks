50e1dc5bed9449c45bd0b3137e7c54e48a4c8a1d
Wayne <rapunzel@Rapunzel.local>
2016-04-17 06:11:13 -0400

    trying to get slider to work

--- goout/app/pages/item-details/item-details.js
+++ goout/app/pages/item-details/item-details.js
@@ -10,8 +10,27 @@ export class ItemDetailsPage {
   }
 
   constructor(nav, navParams) {
+    // private _app: IonicApp
     this.nav = nav;
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedEvent = navParams.get('event');
+
+    // var mySwiper = new Swiper('.swiper-container', {
+    //   speed: 400,
+    //   spaceBetween: 100,
+    //   allowSwipeToNext : false
+    // });
+
+    // mySwiper.allowSwipeToNext = false;
+
+
+  }
+  ngAfterViewInit() {
+    console.log('afteviewinit');
+    console.log(this);
+
+  }
+  goToSlide(slideIndex){
+    this.slider.slider.slideTo(slideIndex);
   }
 }
