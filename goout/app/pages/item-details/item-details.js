801fe0b8e11e61e970103d2cd5260782335003bc
Sandy Barnabas <sbarnabas@gmail.com>
2016-04-17 06:30:33 -0400

    swiper handle

--- goout/app/pages/item-details/item-details.js
+++ goout/app/pages/item-details/item-details.js
@@ -1,36 +1,36 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
-
+import {Page, NavController, NavParams, IonicApp} from "ionic-angular";
 
 @Page({
-  templateUrl: 'build/pages/item-details/item-details.html'
+    templateUrl: 'build/pages/item-details/item-details.html'
 })
 export class ItemDetailsPage {
-  static get parameters() {
-    return [[NavController], [NavParams]];
-  }
+    static get parameters() {
+        return [[NavController], [NavParams], [IonicApp]];
+    }
 
-  constructor(nav, navParams) {
-    // private _app: IonicApp
-    this.nav = nav;
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedEvent = navParams.get('event');
+    constructor(nav, navParams, app) {
+        // private _app: IonicApp
+        this.nav = nav;
+        this._app = app;
+        // If we navigated to this page, we will have an item available as a nav param
+        this.selectedEvent = navParams.get('event');
 
-    // var mySwiper = new Swiper('.swiper-container', {
-    //   speed: 400,
-    //   spaceBetween: 100,
-    //   allowSwipeToNext : false
-    // });
+        // var mySwiper = new Swiper('.swiper-container', {
+        //   speed: 400,
+        //   spaceBetween: 100,
+        //   allowSwipeToNext : false
+        // });
 
-    // mySwiper.allowSwipeToNext = false;
+        // mySwiper.allowSwipeToNext = false;
 
+    }
 
-  }
-  ngAfterViewInit() {
-    console.log('afteviewinit');
-    console.log(this);
+    ngAfterViewInit() {
+        var swiper = this._app.getComponent('my-slides');
+       
+    }
 
-  }
-  goToSlide(slideIndex){
-    this.slider.slider.slideTo(slideIndex);
-  }
+    goToSlide(slideIndex) {
+        this.slider.slider.slideTo(slideIndex);
+    }
 }
